6f0630b9315765ac7b494eef7fd3f435a574aae5
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2023-12-11 17:26:36 +0000

    first push

---
+++ mySQL.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+const port = 3000;
+
+const connectionPool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: 'Maazmysql#1',
+    database: 'solemate_finder'
+});
+
+app.get('/shoe_models/:id?', (req, res) => {
+    const shoeModelsId = req.params.id;
+    let sql;
+
+    if (shoeModelsId) {
+        sql = `SELECT * FROM shoe_model WHERE sku_base = ?`;
+        connectionPool.query(sql, [shoeModelsId], (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                res.status(500).json({ error: 'Internal server error' });
+                return;
+            }
+            res.json(results);
+        });
+    } else {
+        sql = 'SELECT * FROM shoe_model';
+        connectionPool.query(sql, (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                res.status(500).json({ error: 'Internal server error' });
+                return;
+            }
+            res.json(results);
+        });
+    }
+});
+app.get('/shoes/:id?', (req, res) => {
+    const shoeId = req.params.id;
+    let sql;
+
+    if (shoeId) {
+        if(shoeId.toString().includes('-')){
+            sql = `SELECT * FROM shoes, shoe_model, comparison WHERE shoes.sku_base = shoe_model.sku_base AND shoes.sku_full = comparison.sku_full AND shoes.sku_full = ?`;
+            connectionPool.query(sql, [shoeId], (err, results) => {
+                if (err) {
+                    console.error('Error executing query:', err);
+                    res.status(500).json({ error: 'Internal server error' });
+                    return;
+                }
+                res.json(results);
+            });
+        }else{
+            sql = `SELECT * FROM shoes WHERE sku_base = ?`;
+            connectionPool.query(sql, [shoeId], (err, results) => {
+                if (err) {
+                    console.error('Error executing query:', err);
+                    res.status(500).json({ error: 'Internal server error' });
+                    return;
+                }
+                res.json(results);
+            });
+        }
+    } else {
+        sql = 'SELECT * FROM shoes';
+        connectionPool.query(sql, (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                res.status(500).json({ error: 'Internal server error' });
+                return;
+            }
+            res.json(results);
+        });
+    }
+});
+
+app.get('/comparisons', (req, res) => {
+    const sql = 'SELECT * FROM comparison';
+    connectionPool.query(sql, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).json({ error: 'Internal server error' });
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`API server listening at http://localhost:${port}`);
+});
+
+
+
