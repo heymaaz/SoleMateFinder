8991b79989edf95a463d9cd72bfcd696bc001ba7
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2023-12-15 00:23:56 +0000

    ..

--- test/mySQL.test.js
+++ test/mySQL.test.js
@@ -1,12 +1,11 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const { describe, it, before, after } = require('mocha');
-//const app = require('../mySQL');
-const { app, closeDatabaseConnection } = require('../mySQL');
-const e = require('express');
+const chai = require('chai');//import chai
+const chaiHttp = require('chai-http');//import chai-http
+const { describe, it, before, after } = require('mocha');//import mocha
+const { app, closeDatabaseConnection } = require('../mySQL');//import app and closeDatabaseConnection from mySQL.js
+const e = require('express');//import express
 
-chai.use(chaiHttp);
-const expect = chai.expect;
+chai.use(chaiHttp);//use chai-http to make http requests to the server
+const expect = chai.expect;//use expect from chai to make assertions
 
 describe('API Tests', () => {
     before(() => {
@@ -24,41 +23,14 @@ describe('API Tests', () => {
                 });
         });
     });
-/*
-    describe('GET /shoe_models/', () => {
-        it('should return all shoe models if no id is provided', (done) => {
-            chai
-                .request(app)
-                .get('/shoe_models/')
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.an('array');
-                    done();
-                });
-        });
-
-        it('should return a specific shoe model if id is provided', (done) => {
-            chai
-                .request(app)
-                .get('/shoe_models/dj5625')
-                .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.an('array');
-                    expect(res.body[0].sku_base).to.equal('dj5625');
-                    done();
-                });
-        });
-        
-    });
-*/
-    describe('GET /shoes/', () => {
+    describe('GET /shoes/', () => {//
         it('should return all shoes if no id is provided', (done) => {
             chai
                 .request(app)
-                .get('/shoes/')
+                .get('/shoes/')//make get request to /shoes/
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.an('array');
+                    expect(res).to.have.status(200);//expect response to have status code 200
+                    expect(res.body).to.be.an('array');//expect response body to be an array
                     done();
                 });
         });
@@ -66,11 +38,11 @@ describe('API Tests', () => {
         it('should return a specific shoe if id is provided', (done) => {
             chai
                 .request(app)
-                .get('/shoes/dj5625')
+                .get('/shoes/dj5625')//make get request to /shoes/dj5625
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.an('array');
-                    expect(res.body[0].sku_base).to.equal('dj5625');
+                    expect(res).to.have.status(200);//expect response to have status code 200
+                    expect(res.body).to.be.an('array');//expect response body to be an array
+                    expect(res.body[0].sku_base).to.equal('dj5625');//expect sku_base of first shoe in array to be dj5625
                     done();
                 });
         });
@@ -81,20 +53,20 @@ describe('API Tests', () => {
         it('should return the number of shoes in the database', (done) => {
             chai
                 .request(app)
-                .get('/search/count/')
+                .get('/search/count/')//make get request to /search/count/
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body.count).to.be.a('number');
+                    expect(res).to.have.status(200);//expect response to have status code 200
+                    expect(res.body.count).to.be.a('number');//expect response body to have a count property that is a number
                     done();
                 });
         });
         it('should return the number of shoes in the database with a specific Name', (done) => {
             chai
                 .request(app)
-                .get('/search/count/?query=Zoom')
+                .get('/search/count/?query=Zoom')//make get request to /search/count/?query=Zoom
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body.count).to.be.a('number');
+                    expect(res).to.have.status(200);//expect response to have status code 200
+                    expect(res.body.count).to.be.a('number');//expect response body to have a count property that is a number
                     done();
                 });
         });
@@ -104,11 +76,11 @@ describe('API Tests', () => {
         it('should return with 20 shoes if no query is provided. Default pagination limit is 20', (done) => {
             chai
                 .request(app)
-                .get('/search/')
+                .get('/search/')//make get request to /search/
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.an('array');
-                    expect(res.body.length).to.equal(20);
+                    expect(res).to.have.status(200);//expect response to have status code 200
+                    expect(res.body).to.be.an('array');//expect response body to be an array
+                    expect(res.body.length).to.equal(20);//expect response body to have length of 20
                     done();
                 });
         });
@@ -116,11 +88,11 @@ describe('API Tests', () => {
         it('should return all shoes with a specific Name', (done) => {
             chai
                 .request(app)
-                .get('/search/?query=Zoom')
+                .get('/search/?query=Zoom')//make get request to /search/?query=Zoom
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.an('array');
-                    expect(res.body[0].full_name).to.include('Zoom');
+                    expect(res).to.have.status(200);//expect response to have status code 200
+                    expect(res.body).to.be.an('array');//expect response body to be an array
+                    expect(res.body[0].full_name).to.include('Zoom');//expect full_name of first shoe in array to include Zoom
                     done();
                 });
         });
@@ -128,12 +100,12 @@ describe('API Tests', () => {
         it('should return all shoes with a specific Name and limit', (done) => {
             chai
                 .request(app)
-                .get('/search/?query=Air&limit=5')
+                .get('/search/?query=Air&limit=5')//make get request to /search/?query=Air&limit=5
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.an('array');
-                    expect(res.body[0].full_name).to.include('Air');
-                    expect(res.body.length).to.equal(5);
+                    expect(res).to.have.status(200);//expect response to have status code 200
+                    expect(res.body).to.be.an('array');//expect response body to be an array
+                    expect(res.body[0].full_name).to.include('Air');//expect full_name of first shoe in array to include Air
+                    expect(res.body.length).to.equal(5);//expect response body to have length of 5
                     done();
                 });
         });
