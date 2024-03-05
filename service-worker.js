9df90c74fefeabbf8b4aaa2259559dd66ee411c0
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2024-03-05 18:18:04 +0000

    Add manifest file and register service worker

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+var cacheName = "store-v1";
+var cacheFiles = [
+ "index.html"
+];
+self.addEventListener("install", function(e) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log("[Service Worker] Caching files");
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
